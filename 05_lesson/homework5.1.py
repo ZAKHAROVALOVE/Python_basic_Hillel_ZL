a9ab3916f66291bec59a432363aadd2c0e3e06ed
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-04 13:18:24 +0300

    05_lesson. homework5.1

---
+++ 05_lesson/homework5.1.py
@@ -0,0 +1,16 @@
+import keyword
+def is_valid_variable_name(name):
+    if name[0].isdigit() or any(char.isupper() for char in name) or ' ' in name \
+            or any(char in "!\"#$%&'()*+,./:;<=>?@[\\]^`{|}~" for char in name) \
+            or name in keyword.kwlist or (name != '_' and not name.islower()):
+        return False
+    return True
+def print_result(is_valid):
+    print("True" if is_valid else "False")
+def main():
+    name = input("Введіть рядок: ")
+    result = is_valid_variable_name(name)
+    print_result(result)
+
+if __name__ == "__main__":
+    main()
