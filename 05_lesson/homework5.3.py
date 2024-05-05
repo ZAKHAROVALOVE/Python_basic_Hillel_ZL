66ff4a60585f4440021753d5c3c76eb287a29319
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-05 09:14:08 +0300

    fix: # character goes to final tape

--- 05_lesson/homework5.3.py
+++ 05_lesson/homework5.3.py
@@ -1,5 +1,9 @@
+import string
 def generate_hashtag(s):
-    hashtag = '#' + ''.join(word.capitalize() for word in s.split() if word)
-    hashtag = ''.join(filter(str.isalnum, hashtag))[:140]
+    hashtag = ''.join(word.capitalize() for word in s.split() if word)
+    hashtag = ''.join(char for char in hashtag if char not in string.punctuation + ' ')
+    hashtag = hashtag[:140]
+    if hashtag:
+        hashtag = '#' + hashtag
     return hashtag
 print(generate_hashtag(input("Введіть рядок: ")))
