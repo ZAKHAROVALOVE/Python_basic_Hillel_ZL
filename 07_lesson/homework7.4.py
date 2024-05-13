cce488fcf667e07d588ff6ae46228f4ab8184b54
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-13 16:07:00 +0300

    07_lesson homework7.4.

--- 07_lesson/homework7.4.py
+++ 07_lesson/homework7.4.py
@@ -1,9 +1,8 @@
 def common_elements():
-    multiples_of_3 = [num for num in range(1, 101) if num % 3 == 0]
-    multiples_of_5 = [num for num in range(1, 101) if num % 5 == 0]
-    common_elements_set = set(multiples_of_3).intersection(set(multiples_of_5))
-    print("Числа кратні 3:", multiples_of_3)
-    print("Числа кратні 5:", multiples_of_5)
-    print("Перетин чисел:", common_elements_set)
-    pass
-common_elements()
+    multiples_of_3 = [num for num in range(0, 100) if num % 3 == 0]
+    multiples_of_5 = [num for num in range(0, 100) if num % 5 == 0]
+    set_multiples_of_3 = set(multiples_of_3)
+    set_multiples_of_5 = set(multiples_of_5)
+    common_elements_set = set_multiples_of_3.intersection(set_multiples_of_5)
+    return common_elements_set
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
