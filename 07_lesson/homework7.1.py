18fc77cf756b23fa73ce344e9e135d19f9332b6a
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-16 18:35:46 +0300

    fixed 08_lesson homework7.1.

--- 07_lesson/homework7.1.py
+++ 07_lesson/homework7.1.py
@@ -1,6 +1,19 @@
-def say_hi(name, age):
+def say_hi(name: str, age: int) -> str:
+    """
+    Generate a greeting message with the provided name and age.
+
+    Args:
+        name (str): The name of the person.
+        age (int): The age of the person.
+
+    Returns:
+        str: A greeting message containing the name and age of the person.
+    """
     return "Hi. My name is {} and I'm {} years old".format(name, age)
 
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
-assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
-print('ОК')
+
+if __name__ == "__main__":
+    assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+    assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+    print('ОК')
+
