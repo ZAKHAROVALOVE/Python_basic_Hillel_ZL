421b518f1eed54096bf052e243d890a79c4101ba
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-16 14:16:16 +0300

    fixed 08_lesson homework8.2.

--- 08_lesson/homework8.2.py
+++ 08_lesson/homework8.2.py
@@ -1,9 +1,22 @@
-def is_palindrome(text):
+def is_palindrome(text: str) -> bool:
+    """
+    Check if a string is a palindrome.
+
+    Args:
+        text (str): The string to check.
+
+    Returns:
+        bool: True if the string is a palindrome, False otherwise.
+    """
+
     cleaned_text = ''.join(char.lower() for char in text if char.isalnum())
     reversed_text = cleaned_text[::-1]
     return cleaned_text == reversed_text
-assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
-assert is_palindrome('0P') == False, 'Test2'
-assert is_palindrome('a.') == True, 'Test3'
-assert is_palindrome('aurora') == False, 'Test4'
-print("ОК")
+
+
+if __name__ == "__main__":
+    assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+    assert is_palindrome('0P') == False, 'Test2'
+    assert is_palindrome('a.') == True, 'Test3'
+    assert is_palindrome('aurora') == False, 'Test4'
+    print("ОК")
