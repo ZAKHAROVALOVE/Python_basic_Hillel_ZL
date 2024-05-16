d6c57ca68f13cd72f453ec524f62c70167561afe
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-16 13:47:47 +0300

    fixed 08_lesson homework8.1.

--- 08_lesson/homework8.1.py
+++ 08_lesson/homework8.1.py
@@ -1,9 +1,21 @@
 def add_one(some_list):
+    """
+    Increment the number represented by a list of digits by one.
+
+    Args:
+        some_list (list of int): A list of integers representing digits of a number.
+
+    Returns:
+        list of int: A list of integers representing digits of the incremented number.
+    """
     num = int(''.join(map(str, some_list))) + 1
     result_list = [int(digit) for digit in str(num)]
     return result_list
-assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
-assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
-assert add_one([0]) == [1], 'Test3'
-assert add_one([9]) == [1, 0], 'Test4'
-print("ОК")
+
+
+if __name__ == "__main__":
+    assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+    assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+    assert add_one([0]) == [1], 'Test3'
+    assert add_one([9]) == [1, 0], 'Test4'
+    print("ОК")
