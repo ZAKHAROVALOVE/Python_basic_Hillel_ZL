db33cae0d4e5934ec5eb2803ca59ffde947df19b
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-16 19:01:59 +0300

    fixed 07_lesson homework7.4.

--- 07_lesson/homework7.4.py
+++ 07_lesson/homework7.4.py
@@ -1,8 +1,23 @@
-def common_elements():
+def common_elements() -> set:
+    """
+    Finds the common elements between the multiples of 3 and 5 in the range from 0 to 99.
+
+    :return: The set of common elements.
+    :rtype: set
+    """
+
     multiples_of_3 = [num for num in range(0, 100) if num % 3 == 0]
     multiples_of_5 = [num for num in range(0, 100) if num % 5 == 0]
+
     set_multiples_of_3 = set(multiples_of_3)
     set_multiples_of_5 = set(multiples_of_5)
+
     common_elements_set = set_multiples_of_3.intersection(set_multiples_of_5)
+
     return common_elements_set
-assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+
+
+if __name__ == "__main__":
+    assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+    print('OK')
+
