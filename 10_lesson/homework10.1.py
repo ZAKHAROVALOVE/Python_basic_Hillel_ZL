92250f5cd16a25ff0b1071360270e7da00e1d290
Liubov Zakharova <zakharovalove4@gmail.com>
2024-05-17 13:07:49 +0300

    10_lesson homework10.1.

---
+++ 10_lesson/homework10.1.py
@@ -0,0 +1,22 @@
+def some_gen(begin, end, func):
+    """
+    begin: перший елемент послідовності
+    end: кількість елементів у послідовності
+    func: функція, яка формує значення для послідовності
+    """
+    current = begin
+    count = 0
+    while count < end:
+        yield current
+        current = func(current)
+        count += 1
+
+from inspect import isgenerator
+
+def pow(x):
+    return x ** 2
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
